77f2ecf2ac1b20f6fa929af147e645d604930926
Yaniv <soudneko@icloud.com>
2021-03-17 11:12:22 +0200

    post merge commit

--- src/cmps/header.cmp.vue
+++ src/cmps/header.cmp.vue
@@ -1,46 +1,44 @@
 <template>
-	<div class="nav-bar">
-		<div class="logo"><img src="../../materials/icon.png" /></div>
-		<div >
-				<router-link to="/">
-					Explore
-				</router-link>
-				<router-link
-					to="/host"
-				>
-					Become Host
-				</router-link>
-				<router-link
-					to="/login"
-				>
-					Login
-				</router-link>
-		</div>
-	</div>
-			
+  <div class="nav-bar">
+    <div class="logo"><img src="img/app-logo.png" /></div>
+    <div class="links">
+      <router-link to="/"> Explore </router-link>
+      <router-link to="/host"> Become Host </router-link>
+      <router-link to="/login"> Login </router-link>
+    </div>
+  </div>
 </template>
-<style scoped>
-.nav-bar{
-	display: flex;
-    justify-content: space-between;
-    max-width: 1060px;
-    margin: 0 auto;
+
+<style lang="scss" scoped>
+.nav-bar {
+  display: flex;
+  justify-content: space-between;
+  max-width: 1060px;
+  margin: 0 auto;
+  .logo {
+    img {
+      max-width: 50px;
+    }
+  }
+  .links:not(:last-child) {
+    margin-right: 5px;
+  }
 }
 </style>
 
 
 <script>
 export default {
-	name: "NavBar",
-	computed: {
-		user() {
-			return this.$store.getters["user/user"];
-		},
-	},
-	methods: {
-		logout() {
-			this.$store.dispatch({ type: "user/logout" });
-		},
-	},
+  name: "NavBar",
+  computed: {
+    user() {
+      return this.$store.getters["user/user"];
+    },
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch({ type: "user/logout" });
+    },
+  },
 };
 </script>
