f30dbec112cbd78278b185bb8510b0af02d82546
Darkmift <Darkmift@users.noreply.github.com>
2021-03-17 19:02:16 +0200

    added category list

---
+++ src/cmps/categoryList.cmp.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="category-list">
+		<h1>Live anywhere</h1>
+		<div class="categories flex">
+			<div class="category">
+				<img
+					src="https://a0.muscache.com/im/pictures/7d82ca14-56e5-4465-8218-dcfa7d69b6ac.jpg?im_w=480"
+					alt="entire homes"
+				/>
+				<p>Entire Homes</p>
+			</div>
+			<div class="category">
+				<img
+					src="https://a0.muscache.com/im/pictures/7d82ca14-56e5-4465-8218-dcfa7d69b6ac.jpg?im_w=480"
+					alt="entire homes"
+				/>
+				<p>Entire Homes</p>
+			</div>
+			<div class="category">
+				<img
+					src="https://a0.muscache.com/im/pictures/7d82ca14-56e5-4465-8218-dcfa7d69b6ac.jpg?im_w=480"
+					alt="entire homes"
+				/>
+				<p>Entire Homes</p>
+			</div>
+			<div class="category">
+				<img
+					src="https://a0.muscache.com/im/pictures/7d82ca14-56e5-4465-8218-dcfa7d69b6ac.jpg?im_w=480"
+					alt="entire homes"
+				/>
+				<p>Entire Homes</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.category-list {
+	height: 100vh;
+}
+</style>
+
+<script>
+export default { name: "Category-List" };
+</script>
--- src/pages/explorePage.vue
+++ src/pages/explorePage.vue
@@ -2,12 +2,14 @@
 	<div class="explore">
 		<h1>Explore</h1>
 		<explore-list />
+		<category-list />
 	</div>
 </template>
 
 
 <script>
 import exploreList from "../cmps/exploreList.cmp";
+import categoryList from "../cmps/categoryList.cmp";
 export default {
 	name: "Explore",
 	created() {
@@ -15,6 +17,7 @@ export default {
 	},
 	components: {
 		exploreList,
+		categoryList,
 	},
 };
 </script>
