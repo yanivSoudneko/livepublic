313311493cf30bbd06529e594c0979586170fc2e
AriGabay <arigabay19988@gmail.com>
2021-03-17 17:43:31 +0200

    css on explore cityes

--- src/cmps/stayList.cmp.vue
+++ src/cmps/stayList.cmp.vue
@@ -1,34 +1,49 @@
 <template>
-	<div class="stay-list">
-		<div class="stay-preview" v-for="stay in stays" :key="stay._id">
-			{{ stay.loc.address }}
-			<img :src="stay.imgUrls[0]" :alt="stay.name" />
-		</div>
-	</div>
+  <div class="stay-list">
+    <div class="stay-preview" v-for="stay in stays" :key="stay._id">
+      <img class="img-explore-cityes" :src="stay.imgUrls[0]" :alt="stay.name" />
+      <h5>{{ stay.loc.address }}</h5>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .stay-list {
-	.stay-preview {
-		border: 1px solid #fefefe;
-	}
+  height: auto;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 20% 20% 20% 20% 20%;
+  grid-template-rows: 100% 100%;
+  .stay-preview {
+    border: 1px solid #fefefe;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .img-explore-cityes {
+      max-width: 70px;
+      border-radius: 10px;
+      margin: 5px;
+      object-fit: cover;
+      vertical-align: bottom;
+    }
+  }
 }
 </style>
 
 <script>
 export default {
-	name: "stay-list",
-	computed: {
-		stays() {
-			console.log(
-				"🚀 ~ file: stayList.cmp.vue ~ line 16 ~ stayList ~  this.$store.getStays",
-				this.$store.getters["stay/getStays"]
-			);
-			return this.$store.getters["stay/getStays"];
-		},
-	},
-	created() {
-		this.$store.dispatch({ type: "stay/loadStays" });
-	},
+  name: 'stay-list',
+  computed: {
+    stays() {
+      console.log(
+        '🚀 ~ file: stayList.cmp.vue ~ line 16 ~ stayList ~  this.$store.getStays',
+        this.$store.getters['stay/getStays']
+      );
+      return this.$store.getters['stay/getStays'];
+    },
+  },
+  created() {
+    this.$store.dispatch({ type: 'stay/loadStays' });
+  },
 };
 </script>
