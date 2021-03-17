17c0c7d5f0a10d8d1c5b37a1695e968205989265
Darkmift <Darkmift@users.noreply.github.com>
2021-03-17 10:24:26 +0200

    more cmps added

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,40 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <!-- <router-link to="/about">About</router-link> -->
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+		<nav-header />
+		<router-view />
+	</div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+	padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+	a {
+		font-weight: bold;
+		color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+		&.router-link-exact-active {
+			color: #42b983;
+		}
+	}
 }
 </style>
 
 <script>
-import jsonData from '../data/airbnb.json'
+import navHeader from "./cmps/header.cmp";
+import jsonData from "../data/airbnb.json";
 export default {
-  created() {
-    console.log(jsonData);
-  },
-}
+	created() {
+		console.log(jsonData);
+	},
+	components: { navHeader },
+};
 </script>
---
+++ src/cmps/header.cmp.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="header flex j-between a-center">
+		<span>
+			<img src="img/logo.png" alt="" />
+		</span>
+		<span>NAVBAR LINKS placeholder</span>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+	padding: 10px;
+	img {
+		max-width: 50px;
+	}
+}
+</style>
+
+<script>
+export default {
+	name: "Header",
+};
+</script>
