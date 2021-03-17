a90e7b051d2577a5ecb4d088592697b737455600
Yaniv <soudneko@icloud.com>
2021-03-17 10:55:23 +0200

    header cmp

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <!-- <router-link to="/about">About</router-link> -->
+      <nav-bar />
+      
     </div>
-    <router-view/>
+    <!-- <router-view/> -->
   </div>
 </template>
 
@@ -17,25 +17,31 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+// #nav {
+//   padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
 
 <script>
-import jsonData from '../data/airbnb.json'
+import navBar from "./cmps/header.cmp";
 export default {
-  created() {
-    console.log(jsonData);
-  },
-}
+	components: {
+		navBar
+	},
+};
+// import jsonData from '../data/airbnb.json'
+// export default {
+//   created() {
+//     // console.log(jsonData);
+//   },
+// }
 </script>
---
+++ src/cmps/header.cmp.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="nav-bar">
+		<div class="logo"><img src="../../materials/icon.png" /></div>
+		<div >
+				<router-link to="/">
+					Explore
+				</router-link>
+				<router-link
+					to="/host"
+				>
+					Become Host
+				</router-link>
+				<router-link
+					to="/login"
+				>
+					Login
+				</router-link>
+		</div>
+	</div>
+			
+</template>
+<style scoped>
+.nav-bar{
+	display: flex;
+    justify-content: space-between;
+    max-width: 1060px;
+    margin: 0 auto;
+}
+</style>
+
+
+<script>
+export default {
+	name: "NavBar",
+	computed: {
+		user() {
+			return this.$store.getters["user/user"];
+		},
+	},
+	methods: {
+		logout() {
+			this.$store.dispatch({ type: "user/logout" });
+		},
+	},
+};
+</script>
