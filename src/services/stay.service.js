da4845d23d31cd78d64a65ae42c08339a1b2870c
Darkmift <Darkmift@users.noreply.github.com>
2021-03-18 13:22:07 +0200

    added stay service,refactored store

--- src/services/stay.service.js
+++ src/services/stay.service.js
@@ -3,7 +3,7 @@ import { storageService } from './async-storage.service';
 import stayJson from '../../data/stayMockData.json';
 import locationsJson from '../../data/locationsMockData.json';
 
-export const reviewService = {
+export const stayService = {
     queryStays,
     queryLocations,
     getStayById,
@@ -13,17 +13,15 @@ const STAY_KEY = 'stays';
 const LOCATION_KEY = 'locations';
 
 (async () => {
-    if (!gStays) {
-        const stays = await storageService.query(STAY_KEY);
-        if (!stays) {
-            localStorage.setItem(STAY_KEY, stayJson);
-        }
+    console.log('hello??');
+    const stays = await storageService.query(STAY_KEY);
+    if (!stays.length) {
+        localStorage.setItem(STAY_KEY, JSON.stringify(stayJson));
     }
-    if (!gLocations) {
-        const locations = await storageService.query(LOCATION_KEY);
-        if (!locations) {
-            localStorage.setItem(LOCATION_KEY, locationsJson);
-        }
+
+    const locations = await storageService.query(LOCATION_KEY);
+    if (!locations.length) {
+        localStorage.setItem(LOCATION_KEY, JSON.stringify(locationsJson));
     }
 })().catch((err) => {
     console.error('error initialising data!', err);
--- src/store/stay.store.js
+++ src/store/stay.store.js
@@ -1,36 +1,59 @@
 import stayJson from '../../data/stayMockData.json';
 import locationsJson from '../../data/locationsMockData.json';
+
+import { stayService } from '../services/stay.service';
 export default {
-  namespaced: true,
-  state: { stays: [], locations: [] },
-  getters: {
-    getStays(state) {
-      return state.stays;
+    namespaced: true,
+    state: { stays: [], locations: [] },
+    getters: {
+        getStays(state) {
+            return state.stays;
+        },
+        getLocations(state) {
+            return state.locations;
+        },
     },
-    getLocations(state) {
-      return state.locations;
+    mutations: {
+        loadStays(state, { stays }) {
+            state.stays = stays;
+        },
+        loadLocations(state, { locations }) {
+            state.locations = locations;
+        },
+        setStay({ stays }, { stay }) {
+            const idx = stays.findIndex((s) => s._id === stay._id);
+            if (idx === -1) {
+                console.error('🚀 ~  STORE ERROR STAY IDX NOT FOUND', idx);
+                return;
+            }
+            stays = stay;
+        },
     },
-    // getById(state){
-    //     ret
-    // }
-  },
-  mutations: {
-    loadStays(state, { stays }) {
-      state.stays = stays;
+    actions: {
+        async load({ commit }) {
+            try {
+                const stays = await stayService.queryStays();
+                const locations = await stayService.queryLocations();
+                commit({ type: 'loadStays', stays });
+                commit({ type: 'loadLocations', locations });
+            } catch (error) {
+                console.log(
+                    '🚀 ~ STORE ERROR STAY stay.store.js ~ line 30 ~ load ~ error',
+                    error
+                );
+            }
+        },
+        async getById({ commit, state }, { stayId }) {
+            try {
+                const stay = await stayService.getStayById(stayId);
+                commit({ type: 'setStay', stay });
+                return stay;
+            } catch (error) {
+                console.error(
+                    '🚀 ~ STORE ERROR STAY stay.store.js ~ line 43 ~ getById ~ error',
+                    error
+                );
+            }
+        },
     },
-    loadLocations(state, { locations }) {
-      state.locations = locations;
-    },
-  },
-  actions: {
-    load({ commit }) {
-      commit({ type: 'loadStays', stays: stayJson });
-      commit({ type: 'loadLocations', locations: locationsJson });
-    },
-    getById({ commit, state }, { stayId }) {
-      const location = state.stays.filter(stay => stay._id === stayId);
-      console.log({ location, stayId, stays: state.stays });
-      return location ? location[0] : null;
-    },
-  },
 };
