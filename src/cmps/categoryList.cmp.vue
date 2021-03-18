ebefab1b63736c53bc39bf141ab1985123e92a2a
Darkmift <Darkmift@users.noreply.github.com>
2021-03-18 09:01:17 +0200

    fixed bad urls,category list

--- src/cmps/categoryList.cmp.vue
+++ src/cmps/categoryList.cmp.vue
@@ -11,24 +11,24 @@
       </div>
       <div class="category">
         <img
-          src="https://a0.muscache.com/im/pictures/7d82ca14-56e5-4465-8218-dcfa7d69b6ac.jpg?im_w=480"
-          alt="entire homes"
+          src="https://a0.muscache.com/im/pictures/36f53e61-db8d-403c-9122-5b761c0e4264.jpg?im_w=480"
+          alt="Unique stays"
         />
-        <p>Entire Homes</p>
+        <p>Unique stays</p>
       </div>
       <div class="category">
         <img
-          src="https://a0.muscache.com/im/pictures/7d82ca14-56e5-4465-8218-dcfa7d69b6ac.jpg?im_w=480"
-          alt="entire homes"
+          src="https://a0.muscache.com/im/pictures/a596c528-a2e9-4417-9bbe-ceb68b2f6b9c.jpg?im_w=480"
+          alt="Cabins and cottages"
         />
-        <p>Entire Homes</p>
+        <p>Cabins and cottages</p>
       </div>
       <div class="category">
         <img
-          src="https://a0.muscache.com/im/pictures/7d82ca14-56e5-4465-8218-dcfa7d69b6ac.jpg?im_w=480"
-          alt="entire homes"
+          src="https://a0.muscache.com/im/pictures/10a638e1-6aff-4313-8033-1275cec83987.jpg?im_w=480"
+          alt="Pets Allowed"
         />
-        <p>Entire Homes</p>
+        <p>Pets Allowed</p>
       </div>
     </div>
   </div>
