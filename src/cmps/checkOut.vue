695db22d22f9fb9c9027da9964a5c89edaec9df8
Darkmift <Darkmift@users.noreply.github.com>
2021-03-21 12:56:14 +0200

    sticky logic for checkout cmp

--- src/cmps/checkOut.vue
+++ src/cmps/checkOut.vue
@@ -1,151 +1,174 @@
 <template>
-  <div class="check-out">
-    <div class="check-header">
-      <div class="price">${{ stay.price }}/night</div>
-      <div class="rate">
-        <span
-          ><i class="fas fa-star">{{ rating }}</i></span
-        >&nbsp;
-        <span>{{ ratingLength }}</span>
-      </div>
-    </div>
-    <form @submit.prevent="checkout">
-      <div class="checkout-input">
-        <!-- dates -->
-        <date-picker placeholder="Check In" @emitDate="setDates($event)" />
-        <!-- guest count -->
-        <div class="guest-count">
-          <input
-            type="number"
-            placeholder="Guests"
-            :v-show="order.guest"
-            v-model="order.guest"
-            :max="stay.accommodates"
-          />
-        </div>
-        <button
-          class="check"
-          @mousemove="recordPos"
-          :style="{ backgroundImage: calculatedPos }"
-        >
-          Check Availability
-        </button>
-        <div v-if="showSummary === 2">Hi</div>
-      </div>
-    </form>
-  </div>
+	<div
+		class="check-out"
+		:style="{ top: stickyYPos }"
+		v-if="scrollPosition && scrollPosition > 1000"
+	>
+		<div class="check-header">
+			<div class="price">${{ stay.price }}/night</div>
+			<div class="rate">
+				<span
+					><i class="fas fa-star">{{ rating }}</i></span
+				>&nbsp;
+				<span>{{ ratingLength }}</span>
+			</div>
+		</div>
+		<form @submit.prevent="checkout">
+			<div class="checkout-input">
+				<!-- dates -->
+				<date-picker
+					placeholder="Check In"
+					@emitDate="setDates($event)"
+				/>
+				<!-- guest count -->
+				<div class="guest-count">
+					<input
+						type="number"
+						placeholder="Guests"
+						:v-show="order.guest"
+						v-model="order.guest"
+						:max="stay.accommodates"
+					/>
+				</div>
+				<button
+					class="check"
+					@mousemove="recordPos"
+					:style="{ backgroundImage: gradient }"
+				>
+					Check Availability
+				</button>
+				<div v-if="showSummary === 2">Hi</div>
+			</div>
+		</form>
+	</div>
 </template>
 
 <style lang="scss">
 .check-out {
-  // position: fixed;
-  position: sticky;
-  top: 0;
-  //////
-  float: right;
-  right: 250px;
-  margin-top: 55px;
-  float: right;
-  border: 1px solid rgb(221, 221, 221);
-  border-radius: 12px;
-  padding: 24px;
-  box-shadow: rgba(0, 0, 0, 0.35) 0px 6px 16px;
-  // display: flex;
-  // flex-direction: column;
-  // width: 280px;
+	position: fixed;
+	// position: sticky;
+	top: 0;
+	//////
+	float: right;
+	right: 250px;
+	margin-top: 55px;
+	float: right;
+	border: 1px solid rgb(221, 221, 221);
+	border-radius: 12px;
+	padding: 24px;
+	box-shadow: rgba(0, 0, 0, 0.35) 0px 6px 16px;
+	// display: flex;
+	// flex-direction: column;
+	// width: 280px;
 
-  .check-header {
-    display: flex;
-    justify-content: space-between;
-  }
-  .check {
-    cursor: pointer;
-    font-size: 16px !important;
-    line-height: 20px !important;
-    font-weight: 600 !important;
-    border-radius: 8px !important;
-    outline: none !important;
-    padding: 14px 24px !important;
-    // background-position: calc((100 - var(--mouse-x, 0)) * 1%) calc((100 - var(--mouse-y, 0)) * 1%);
-    // --mouse-x: 85.3438;
-    // --mouse-y: 52.9412;
-    transition: box-shadow 0.2s ease 0s, -ms-transform 0.1s ease 0s,
-      -webkit-transform 0.1s ease 0s, transform 0.1s ease 0s !important;
-    border: none !important;
-    background: linear-gradient(
-      to right,
-      rgb(230, 30, 77) 0%,
-      rgb(227, 28, 95) 50%,
-      rgb(215, 4, 102) 100%
-    );
-    color: white;
-  }
+	.check-header {
+		display: flex;
+		justify-content: space-between;
+	}
+	.check {
+		cursor: pointer;
+		font-size: 16px !important;
+		line-height: 20px !important;
+		font-weight: 600 !important;
+		border-radius: 8px !important;
+		outline: none !important;
+		padding: 14px 24px !important;
+		// background-position: calc((100 - var(--mouse-x, 0)) * 1%) calc((100 - var(--mouse-y, 0)) * 1%);
+		// --mouse-x: 85.3438;
+		// --mouse-y: 52.9412;
+		transition: box-shadow 0.2s ease 0s, -ms-transform 0.1s ease 0s,
+			-webkit-transform 0.1s ease 0s, transform 0.1s ease 0s !important;
+		border: none !important;
+		background: linear-gradient(
+			to right,
+			rgb(230, 30, 77) 0%,
+			rgb(227, 28, 95) 50%,
+			rgb(215, 4, 102) 100%
+		);
+		color: white;
+	}
 }
 </style>
 
 <script>
 import datePicker from "../cmps/datepicker.cmp";
 export default {
-  props: {
-    stay: {
-      type: Object,
-      Request,
-    },
-  },
-  name: "checkOut",
-  data() {
-    return {
-      mouseX: 0,
-      mouseY: 0,
-      order: {
-        checkIn: null,
-        checkOut: null,
-        guest: 1,
-      },
-      showSummary: 0,
-    };
-  },
-  methods: {
-    setDates(ev) {
-      this.order.checkIn = ev[0];
-      this.order.checkOut = ev[1];
-      this.showSummary++;
-      console.log(
-        "🚀 ~ file: checkOut.vue ~ line 114 ~ setDates ~  this.showSummary",
-        this.showSummary
-      );
-    },
-    recordPos(ev) {
-      const { layerX, layerY } = ev;
-      this.mouseX = layerX;
-      this.mouseY = layerY;
-    },
-    checkout() {
-      // this.order.guest is String !!
-      this.order.guest = +this.order.guest;
-      this.$emit("checkout", this.order);
-    },
-  },
-  computed: {
-    calculatedPos() {
-      return `radial-gradient(at ${this.mouseX}% ${this.mouseY}%, #e61e4d, #9b59b6)`;
-    },
-    rating() {
-      const reviews = this.stay.reviews;
-      const rateTotal = reviews.reduce((acc, obj) => {
-        acc += obj.rate;
-        return acc;
-      }, 0);
-      return (rateTotal / reviews.length).toFixed(1);
-    },
-    ratingLength() {
-      const reviewsLength = this.stay.reviews.length;
-      const addS = reviewsLength > 1 ? "s" : "";
-      const string = reviewsLength + " Review" + addS;
-      return string;
-    },
-  },
-  components: { datePicker },
-  created() {},
+	props: {
+		stay: {
+			type: Object,
+			Request,
+		},
+	},
+	name: "checkOut",
+	data() {
+		return {
+			mouseX: 0,
+			mouseY: 0,
+			order: {
+				checkIn: null,
+				checkOut: null,
+				guest: 1,
+			},
+			showSummary: 0,
+			scrollPosition: null,
+		};
+	},
+	methods: {
+		setDates(ev) {
+			this.order.checkIn = ev[0];
+			this.order.checkOut = ev[1];
+			this.showSummary++;
+			console.log(
+				"🚀 ~ file: checkOut.vue ~ line 114 ~ setDates ~  this.showSummary",
+				this.showSummary
+			);
+		},
+		recordPos(ev) {
+			const { layerX, layerY } = ev;
+			this.mouseX = layerX;
+			this.mouseY = layerY;
+		},
+		checkout() {
+			// this.order.guest is String !!
+			this.order.guest = +this.order.guest;
+			this.$emit("checkout", this.order);
+		},
+		updateScroll() {
+			this.scrollPosition = window.scrollY;
+			console.log(
+				"🚀 ~ file: checkOut.vue ~ line 133 ~ updateScroll ~ 	this.scrollPosition",
+				this.scrollPosition
+			);
+		},
+	},
+	computed: {
+		stickyYPos() {
+			return this.scrollPosition;
+		},
+		gradient() {
+			return `radial-gradient(at ${this.mouseX}% 50%, #e61e4d, #9b59b6)`;
+		},
+		rating() {
+			const reviews = this.stay.reviews;
+			const rateTotal = reviews.reduce((acc, obj) => {
+				acc += obj.rate;
+				return acc;
+			}, 0);
+			return (rateTotal / reviews.length).toFixed(1);
+		},
+		ratingLength() {
+			const reviewsLength = this.stay.reviews.length;
+			const addS = reviewsLength > 1 ? "s" : "";
+			const string = reviewsLength + " Review" + addS;
+			return string;
+		},
+	},
+	components: { datePicker },
+	mounted() {
+		window.addEventListener("scroll", this.updateScroll);
+	},
+	destroyed() {
+		window.removeEventListener("scroll", this.updateScroll);
+	},
 };
 </script>
