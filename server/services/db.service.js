1714fae4e052341411bd220c3135c430fd83b53e
AriGabay <arigabay19988@gmail.com>
2021-03-22 15:37:35 +0200

    backend-basic

--- server/services/db.service.js
+++ server/services/db.service.js
@@ -3,7 +3,7 @@ const logger = require('./logger.service');
 const config = require('../config');
 
 module.exports = {
-    getCollection,
+  getCollection,
 };
 
 // Database Name
@@ -12,28 +12,28 @@ const dbName = process.env.DB_NAME;
 var dbConn = null;
 
 async function getCollection(collectionName) {
-    try {
-        const db = await _connect();
-        const collection = await db.collection(collectionName);
-        return collection;
-    } catch (err) {
-        logger.error('Failed to get Mongo collection', err);
-        throw err;
-    }
+  try {
+    const db = await _connect();
+    const collection = await db.collection(collectionName);
+    return collection;
+  } catch (err) {
+    logger.error('Failed to get Mongo collection', err);
+    throw err;
+  }
 }
 
 async function _connect() {
-    if (dbConn) return dbConn;
-    try {
-        const client = await MongoClient.connect(config.dbURL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        const db = client.db(dbName);
-        dbConn = db;
-        return db;
-    } catch (err) {
-        logger.error('Cannot Connect to DB', err);
-        throw err;
-    }
+  if (dbConn) return dbConn;
+  try {
+    const client = await MongoClient.connect(config.dbURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const db = client.db(dbName);
+    dbConn = db;
+    return db;
+  } catch (err) {
+    logger.error('Cannot Connect to DB', err);
+    throw err;
+  }
 }
