b2f7b99e5e7b80bc6f49ea89e0c7f915aeb5f64c
Darkmift <Darkmift@users.noreply.github.com>
2021-03-26 16:37:02 +0300

    added dropdown

--- client/src/store/user.store.js
+++ client/src/store/user.store.js
@@ -61,6 +61,7 @@ export default {
         },
         checkStoredUser({ commit }) {
             const resUser = userService.checkStoredUser();
+            if (!resUser) return;
             commit({ type: 'setUser', user: resUser });
         },
     },
